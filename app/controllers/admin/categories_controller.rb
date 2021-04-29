9472bc438be92d43e0ec3601a528a0b8701067af
Adele Noronha <adele.c.noronha@gmail.com>
2021-04-29 23:19:34 +0000

    Add categories controller, index and new

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
