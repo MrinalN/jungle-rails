094bae74aa0451ca6546ad606d937a8f43ca8fb8
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-01 18:46:22 +0000

    Correct ref errors

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,20 +6,20 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def create
-    @categories = Category.new(category_params)
+    @category = Category.new(category_params)
 
-    if @categories.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end
 
-
+  private
 
   def category_params
     params.require(:category).permit(:name)
