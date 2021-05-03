683e5ab1e3fe4124bf7981e2e009161f6d3ddba1
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-03 23:43:03 +0000

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  def authenticate_with_credentials
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, :case_sensitive => false
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
   
 end
