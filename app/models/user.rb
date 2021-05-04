d655f59a81d141209409db21e839de584a07151d
Adele Noronha <adele.c.noronha@gmail.com>
2021-05-04 00:20:46 +0000

    Add min passowrd length

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, :case_sensitive => false
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
 end
